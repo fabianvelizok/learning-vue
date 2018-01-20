fa2d30804e5a45940d599be39c48c11334e08e46
Fabián Veliz <velizfabianhoracio@gmail.com>
2018-01-20 18:17:34 -0300

    Fix wrong paths

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import config from './config';
 
 export default function getArtists (country) {
-  const url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${config.API_KEY}&format=json`;
+  const url = `https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=${country}&api_key=${config.API_KEY}&format=json`;
 
   return fetch(url)
     .then(response => response.json())
